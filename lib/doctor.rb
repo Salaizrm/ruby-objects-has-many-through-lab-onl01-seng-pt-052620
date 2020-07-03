1a8371df3bc8032ebd7ea6337b8941e1b21e20a9
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-03 02:30:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -3,4 +3,16 @@ class Doctor
   attr_accessor :name
   @@all = []
 
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
