8891e39e2fe6bfdc67739ccabff54cf0cac9ce51
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-03 02:15:48 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,10 @@
 class Genre
 
-  attr_accessor :genre
+  attr_accessor :name
   @@all = []
 
-  def initialize(genre)
-    @genre = genre
+  def initialize(name)
+    @name = name
     save
   end
 
