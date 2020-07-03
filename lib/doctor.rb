cd514836dd34262a92d4ac534325a550f1c99263
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-03 02:42:20 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,7 +19,7 @@ class Doctor
   def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end
-  
+
   def appointments
     Appointment.all.select do |x|
       x.doctor == self
